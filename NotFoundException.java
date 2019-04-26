d271e829cc6eb1ec147672cb042a6186206a83ef
timurxo <45484568+timurxo@users.noreply.github.com>
2019-04-26 17:48:12 -0500

    Update NotFoundException.java

--- NotFoundException.java
+++ NotFoundException.java
@@ -1,4 +1,9 @@
 package final_project;
 
 public class NotFoundException extends Exception {
+	
+	public NotFoundException()
+	{
+	System.out.println("Such a user name does not exist in the system!");
+	}
 }
