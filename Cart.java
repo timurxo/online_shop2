fe9cdbf5551f81f679c2d60070e6c9282a3044ae
tiara28307 <36643475+tiara28307@users.noreply.github.com>
2019-04-29 20:21:22 -0500

    Update Cart.java

--- Cart.java
+++ Cart.java
@@ -1,58 +1,54 @@
+package final_project;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
-// CATALOG
-
-// select items
-// place order
-
-public class Cart {
-    private ArrayList<Item> availableProducts;
-    private ArrayList<Item> cartItems;
+public class Cart
+{
+	private ArrayList<Item> availableProducts = new ArrayList<>();
+    private ArrayList<Item> cartItems = new ArrayList<>();;
 
 
-    public Cart() {
-        availableProducts.add(new Item("TV", 1700));
-        availableProducts.add(new Item("PS4", 399));
-        availableProducts.add(new Item("Xbox", 299));
-        availableProducts.add(new Item("Witcher 3", 59));
-        availableProducts.add(new Item("Red Dead Redemption", 49));
-        availableProducts.add(new Item("Black Ops", 59));
-        availableProducts.add(new Item("Pre-Build Desktop computer", 2000));
-        availableProducts.add(new Item("Keyboard", 35));
-        availableProducts.add(new Item("Mouse", 25));
-        availableProducts.add(new Item("1440p Monitor", 300));
+    public Cart() 
+    {
+   			this.availableProducts.add(new Item("TV", 1700, 3));
+   			this.availableProducts.add(new Item("PS4", 399, 10));
+   			this.availableProducts.add(new Item("Xbox", 299, 3));
+   			this.availableProducts.add(new Item("Witcher 3", 59, 20));
+   			this.availableProducts.add(new Item("Red Dead Redemption", 49, 1));
+   			this.availableProducts.add(new Item("Black Ops", 59, 200));
+   			this.availableProducts.add(new Item("Pre-Build Desktop computer", 2000, 4));
+   			this.availableProducts.add(new Item("Keyboard", 35, 16));
+   			this.availableProducts.add(new Item("Mouse", 25, 10));
+   			this.availableProducts.add(new Item("1440p Monitor", 300, 25));
+    		
     }
 
 
-    // INTERACTION WITH BANK REGARDING VERIFICATION OF OPERATION
-    public Order checkout() throws UnauthorizedException {
-
+    public Order checkout() throws UnauthorizedException 
+    {
         User user = Session.getInstance().getUser();
         Bank bank = new Bank();
         Boolean verified = bank.verifyCard(user.getCard(), getCartTotal());
 
-        // IF NOT VERIFIED BY BANK
+        // if information is incorrect
         if (!verified) {
             throw new UnauthorizedException();
-        }
-
-        // IF VERIFIED BY BANK -> create order
-        else
-            {
+        } 
+        else // if everything is verified
+        {
+        	System.out.println("Welcome to checkout");
             Order order = new Order(user.getId(), cartItems, getCartTotal());
             return order;
         }
     }
-
-
-
-    // USER SELECTS ITEMS AND ADDS THEM TO CART
-    public void addItemsToCart() {
+    
+    public void addItemsToCart() 
+    {
         Scanner sc = new Scanner(System.in);
 
         for (int i=0; i < availableProducts.size(); i++) {
-            System.out.println(i + ". " + availableProducts.get(i).getName() + " - $" + availableProducts.get(i).getPrice());
+            System.out.println(i + ". " + availableProducts.get(i).getName() + " - $" + availableProducts.get(i).getPrice() + " (amount: " + availableProducts.get(i).getQuantity() + ")\n");
         }
 
 
@@ -64,44 +60,53 @@ public class Cart {
 
 
         // keep asking for selection of other items
-        while (true) {
+        while (true) 
+        {
+        	sc.nextLine();
             System.out.println("Do you need something else? (yes/no)");
 
             String yn = sc.nextLine();
 
             if (yn.equals("yes") || yn.equals("y")) {
-                System.out.println("Select an item by entering it's number ");
+                System.out.println("Select an item by entering it's number: ");
                 choice = sc.nextInt();
                 cartItems.add(availableProducts.get(choice));
-
             } else if (yn.equals("no")) {
-                System.out.println("You've chosen: " + cartItems);
-                break;
-                
-                System.out.println("You have chosen: ");
+
+
+                System.out.println("You've chosen: ");
                 for (Item cartItem : cartItems) {
                     System.out.println(cartItem.getName());
                 }
-                
+                break;
             }
         }
+        
+        sc.close();
     }
-
-
+    
+    
 
     public ArrayList<Item> getCart() {
         return cartItems;
     }
-
-
+    
     public Integer getCartTotal() {
-
+        
+        
+        // verifyCard(int card, int price)
+        // 
         Integer total = 0;
-
-           for (Item cartItem : cartItems) {
-               total += cartItem.getPrice();
-           }
-           return total;
-
+        
+	    // if the amount on card is exceeded
+     //   if (verifyCard(88, 445))
+        
+        for (Item cartItem : cartItems) {
+            total += cartItem.getPrice();
+        }
+        return total;
     }
+    
+    
+
 }
